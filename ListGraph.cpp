a96b5ae6e97fdcaf4a27f946593ef5ce49452fbd
Mike Marsden <marsdems@muohio.edu>
2012-12-02 21:35:28 -0500

    Work on MatrixGraph and ListGraph

--- ListGraph.cpp
+++ ListGraph.cpp
@@ -1,1 +1,38 @@
-// ListGraph.cpp Template
+/**********************************************************
+* Mike Marsden
+* HW05 - Traveling Sales Person 
+* ListGraph.cpp
+**********************************************************/
+#include "ListGraph.h"
+
+//Constructor
+ListGraph::ListGraph(int numNodes) {
+
+}
+
+//Destructor
+ListGraph::~ListGraph() { }
+
+void ListGraph::addEdge(NodeID u, Node v, EdgeWeight weight) {
+
+}
+
+EdgeWeight ListGraph::weight(NodeID u, NodeID v) const {
+
+}
+
+std::list<NWPair> ListGraph::getAdj(NodeID u) const {
+
+}
+
+unsigned ListGraph::degree(NodeID u) const {
+
+}
+
+unsigned ListGraph::size() const {
+	return edgeList.size();
+}
+
+unsigned ListGraph::numEdges() const {
+	return num_edges;
+}
--- MatrixGraph.cpp
+++ MatrixGraph.cpp
@@ -1,1 +1,35 @@
-//MatrixGraph.cpp
+/**********************************************************
+* Mike Marsden
+* HW05 - Traveling Sales Person 
+* MatrixGraph.cpp
+**********************************************************/
+#include "MatrixGraph.h"
+
+//Constructor
+MatrixGraph::MatrixGraph(unsigned num_nodes) {
+
+}
+
+MatrixGraph::~MatrixGraph() {}
+
+//Setter Methods
+void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight) {
+
+}
+
+//Accessors
+EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const {
+
+}
+
+unsigned MatrixGraph::degree(NodeID u) const {
+
+}
+
+unsigned MatrixGraph::size() const {
+	M.size();
+}
+
+unsigned MatrixGraph::numEdges() const {
+	return num_edges;
+}
