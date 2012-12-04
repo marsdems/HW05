74b949ccaa9c3b7086a481be9eff4dd4c1163a8d
Mike Marsden <marsdems@muohio.edu>
2012-12-03 21:45:34 -0500

    MatrixGraph Complete

--- MatrixGraph.cpp
+++ MatrixGraph.cpp
@@ -9,6 +9,11 @@
 MatrixGraph::MatrixGraph(unsigned num_nodes) {
 
 	num_edges = 0;
+	M.resize(num_nodes);
+
+	for (int i = 0; i < M.size(); i++) {
+		M[i].resize(num_nodes);
+	}
 }
 
 //Destructor
@@ -17,6 +22,10 @@ MatrixGraph::~MatrixGraph() {}
 //Setter Methods
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight) {
 
+	// By going u,v and v,u, it makes it undirected.
+	M[u][v] = weight;
+	M[v][u] = weight;
+	num_edges++;
 }
 
 //Accessors
@@ -26,12 +35,28 @@ EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const {
 }
 
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const {
+	std::list<NWPair> adjs = std::list<NWPair>();
+	for (int i = 0; i < M.size(); i++) {
+		// If this condition is met, there is a weight
+		// therefore an adjacent point.
+		if (M[u][i] != 0) {
+			adjs.push_back(NWPair(i, M[u][i]));
+		}
+	}
 
+	return adjs;
 }
 
 unsigned MatrixGraph::degree(NodeID u) const {
-
-}
+	unsigned degree = 0;
+	for (int i = 0; i < M.size(); i++) {
+		// Will increment if a weight is found
+		if (M[u][i] != 0) {
+			degree++;
+		}
+	}		
+	return degree;
+} 
 
 unsigned MatrixGraph::size() const {
 	return M.size();
